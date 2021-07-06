7dd03175814c9319322fa441ac576dc9b58234b4
ennunpenoctal <ennunpentoctal@protonmail.com>
2021-07-06 12:23:17 +0000

    Dispatch message, config read commands, status command

--- klaimera.py
+++ klaimera.py
@@ -15,6 +15,7 @@ import klogging
 import kutils
 
 MUDAE_AID = 432610292342587392
+KLAIMERA_START = time()
 logger = klogging
 
 
@@ -118,10 +119,20 @@ class EventManager:
     ) -> None:
         if isinstance(at, timedelta):
             timestamp = int(time() + at.total_seconds())
+            time_info = datetime.now() + at
         elif isinstance(at, datetime):
             timestamp = int(at.timestamp())
+            time_info = at
         else:
             timestamp = at
+            time_info = datetime.fromtimestamp(at)
+
+        if recur:
+            recur_info = f", recurring every {delta}."
+        else:
+            recur_info = "."
+
+        logger.info(f"Dispatched {type} for {time_info}{recur_info}")
 
         self.events.append(
             Event(
@@ -150,69 +161,111 @@ class Klaimera(discord.Client):
         else:
             base, args = scmd
 
-        print(base, args)
-
         if base == "config":
             # kmra config targets.character add "Yuki Nagato"
             #      <base> <0--------------- 1-- 2------------
             if args and len(sarg := args.split(" ")) >= 1:
                 if len(sarg) == 1:
                     if sarg[0] == "reload":
-                        await self.config.load()
-                        await logger.info("Reloaded configuration")
+                        try:
+                            await self.config.load()
+                        except Exception as err:
+                            await logger.warn("Error reloading configuration", err=err)
+                            return 2
+                        else:
+                            await logger.info("Reloaded configuration")
+                            return 0
 
-                        warn_message_str = "\n".join(
-                            [f"    {mesg}" for mesg in self.config.commands_warnMessage]
-                        )
-
-                        await message.reply(
-                            (
-                                "```toml"
-                                "\n[user]\n"
-                                f"notify = {self.config.user_notify}\n"
-                                f"sound = {self.config.user_sound}\n"
-                                "\n[commands]\n"
-                                f"enable       = {self.config.commands_enable}\n"
-                                f"status       = {self.config.commands_status}\n"
-                                f"config       = {self.config.commands_config}\n"
-                                f"dispatch     = {self.config.commands_dispatch}\n"
-                                f"notify       = {self.config.commands_notify}\n"
-                                f"log          = {self.config.commands_log}\n"
-                                f"emoji        = {self.config.commands_emoji}\n"
-                                f"emojiSuccess = {self.config.commands_emojiSuccess}\n"
-                                f"emojiFailure = {self.config.commands_emojiFailure}\n"
-                                f"emojiInvalid = {self.config.commands_emojiInvalid}\n"
-                                f"warnMessage  = [\n"
-                                f"{warn_message_str}"
-                                "]\n"
-                                "\n[dispatch.roll]\n"
-                                "\n[dispatch.claim]\n"
-                                "\n[target]\n"
-                                "\n[server]\n"
-                                "\n[server.settings]\n"
-                                "```"
-                            )
-                        )
+                    elif sarg[0] in self.config.idmap:
+                        await message.reply(f"```\n{self.config.idmap[sarg[0]]}\n```")
+                        return -1
 
                     else:
+                        return 1
+
+                elif len(sarg) == 3 and sarg[0] in self.config.idmap:
+                    # TODO: Configuration write commands
+                    if sarg[1] == "set":
                         ...
                         return 0
 
-                elif len(sarg) == 3:
-                    ...
-                    return 0
+                    elif sarg[1] == "add":
+                        ...
+                        return 0
+
+                    elif sarg[1] == "rem":
+                        ...
+                        return 0
+
+                    else:
+                        return 1
 
                 else:
                     return 1
 
             elif not args:
-                ...
-                return 0
+                warn_message_str = "\n".join(
+                    [f"    {mesg}," for mesg in self.config.commands_warnMessage]
+                )
+                target_character_str = "\n".join(
+                    [f"    {mesg}," for mesg in self.config.target_character]
+                )
+                target_series_str = "\n".join(
+                    [f"    {mesg}," for mesg in self.config.target_series]
+                )
+
+                await message.reply(
+                    (
+                        "```"
+                        "\n[user]\n"
+                        f"notify = {self.config.user_notify}\n"
+                        f"sound  = {self.config.user_sound}\n"
+                        "\n[commands]\n"
+                        f"enable       = {self.config.commands_enable}\n"
+                        f"status       = {self.config.commands_status}\n"
+                        f"config       = {self.config.commands_config}\n"
+                        f"dispatch     = {self.config.commands_dispatch}\n"
+                        f"notify       = {self.config.commands_notify}\n"
+                        f"log          = {self.config.commands_log}\n"
+                        f"emoji        = {self.config.commands_emoji}\n"
+                        f"emojiSuccess = {self.config.commands_emojiSuccess}\n"
+                        f"emojiFailure = {self.config.commands_emojiFailure}\n"
+                        f"emojiInvalid = {self.config.commands_emojiInvalid}\n"
+                        "warnMessage  = [\n"
+                        f"{warn_message_str}"
+                        "\n]\n"
+                        "\n[dispatch.roll]\n"
+                        f"auto    = {self.config.dispatch_roll_auto}\n"
+                        f"command = {self.config.dispatch_roll_command}\n"
+                        "\n[dispatch.claim]\n"
+                        f"auto      = {self.config.dispatch_claim_auto}\n"
+                        f"threshold = {self.config.dispatch_claim_threshold}\n"
+                        f"delay     = {self.config.dispatch_claim_delay}\n"
+                        f"emoji     = {self.config.dispatch_claim_emoji}"
+                        "\n[target]\n"
+                        f"character = [\n"
+                        f"{target_character_str}"
+                        "\n]\n"
+                        f"series    = [\n"
+                        f"{target_series_str}"
+                        "\n]\n"
+                        "\n[server]\n"
+                        f"channel = {self.config.server_channel}\n"
+                        "\n[server.settings]\n"
+                        f"claim       = {self.config.server_settings_claim}\n"
+                        f"claimExpire = {self.config.server_settings_claimExpire}\n"
+                        f"claimReset  = {self.config.server_settings_claimReset}\n"
+                        f"claimAnchor = {self.config.server_settings_claimAnchor}\n"
+                        f"rolls       = {self.config.server_settings_rolls}\n"
+                        "```"
+                    )
+                )
+                return -1
 
             else:
                 return 1
 
-        if base == "dispatch":
+        elif base == "dispatch":
             # kmra dispatch roll 0
             if args is None:
                 await message.reply(str(self.eventmgr.events))
@@ -224,7 +277,7 @@ class Klaimera(discord.Client):
             else:
                 return 1
 
-        if base == "notify":
+        elif base == "notify":
             if args == "push":
                 ...
                 return 0
@@ -236,7 +289,7 @@ class Klaimera(discord.Client):
             else:
                 return 1
 
-        if base == "log":
+        elif base == "log":
             if args and ":" in args:
                 ...
                 return 0
@@ -247,6 +300,20 @@ class Klaimera(discord.Client):
 
             else:
                 return 1
+
+        elif base == "status":
+            kmra_start_dt = datetime.fromtimestamp(KLAIMERA_START)
+            uptime = datetime.now() - kmra_start_dt
+
+            await message.reply(
+                f"klaimera is running, and has been for `{uptime}`."
+                f" (since `{kmra_start_dt}`)"
+            )
+
+            return -1
+
+        else:
+            return 1
 
         return 0
 
@@ -267,10 +334,10 @@ class Klaimera(discord.Client):
                     await self.config.load()
 
                 except Exception as exc:
-                    await logger.warn("Unsuccessfull config reload", exc=exc)
+                    await logger.warn("Unsuccessful config reload", exc=exc)
 
         await self.eventmgr.dispatch(
-            type=EventType.ROLL,
+            type=EventType.RELOAD,
             call=reloader,
             at=timedelta(hours=1),
             recur=True,
